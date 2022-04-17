21bae60fb795d8d23616dd0be015f416ca823bdf
Sarita <sm7767119@gmail.com>
2022-04-17 09:14:21 +0530

    DateTime

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const name="Sarita Maurya";
+const currDate=new Date().toLocaleDateString();
+const currTime=new Date().toLocaleTimeString();
+
+ReactDOM.render(
+  <div>
+  <h1>Hello, My name is {name}</h1>
+  <p>Current date is ={currDate}</p>
+  <p>Current time is ={currTime}</p>
+  </div>,
+  document.getElementById('root')
+  
+
+
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
